334d785b442689666c6c2135bd115bc6ef145917
Zhongyu-Cui <czyzyys1990n@outlook.com>
2025-10-29 10:19:10 +0000

    Update main.py

--- DecisionTreeCourseworkProject/main.py
+++ DecisionTreeCourseworkProject/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from src.decision_tree import DecisionTree
 from src.evaluation import cross_validate_decision_tree, cross_validate_pruned_tree
-# from src.pruning import prune_tree
+from src.visualization import plot_decision_tree
 import argparse
 
 def load_dataset(path):
@@ -22,8 +22,6 @@ def parse_args():
                         help="Max depth of tree")
     parser.add_argument("--threshold", type=float, default=0.01,
                         help="Threshold for Information Gain determining whether a node is leaf node")
-    parser.add_argument("--show", action="store_true",
-                        help="Show the plot of trees visualized")
     return parser.parse_args()
 
 def main():
@@ -36,32 +34,36 @@ def main():
     X = data[:, :-1]
     y = np.round(data[:, -1]).astype(int)
 
-    # Before pruning
-    print("\n=== Before pruning ===")
-    res_before = cross_validate_decision_tree(
-        X, y, DecisionTree, filename=filename, show_trees=args.show,
-        n_splits=args.folds, seed=args.seed,
-        max_depth=args.max_depth, threshold=args.threshold
-    )
-    print(f"\n=== {args.dataset} | {args.folds}-fold CV ===")
-    print("Labels:", res_before["labels"])
-    print("Confusion Matrix (single 4×4):\n", res_before["confusion_matrix"])
-    print(f"Accuracy (from aggregated CM): {res_before['accuracy_from_cm']:.4f}")
-    print(
-        f"Accuracy (mean±std over folds): {res_before['accuracy_mean_over_folds']:.4f} ± {res_before['accuracy_std_over_folds']:.4f}")
-    for i, lab in enumerate(res_before["labels"]):
-        print(f"Class {lab}  Precision: {res_before['precision_per_class'][i]:.4f}  "
-              f"Recall: {res_before['recall_per_class'][i]:.4f}  F1: {res_before['f1_per_class'][i]:.4f}")
-    print(f"Macro Precision: {res_before['macro_precision']:.4f}  "
-          f"Macro Recall: {res_before['macro_recall']:.4f}  Macro F1: {res_before['macro_f1']:.4f}")
-    print()
+    # No pruning, train and evaluate
+    if not args.prune:
+        print("\n=== No pruning to be applied ===")
+        res_before = cross_validate_decision_tree(
+            X, y, DecisionTree, filename=filename,
+            n_splits=args.folds, seed=args.seed,
+            max_depth=args.max_depth, threshold=args.threshold
+        )
+        print(f"\n=== {args.dataset} | {args.folds}-fold CV ===")
+        print("Labels:", res_before["labels"])
+        print("Confusion Matrix (single 4×4):\n", res_before["confusion_matrix"])
+        print(f"Accuracy (from aggregated CM): {res_before['accuracy_from_cm']:.4f}")
+        print(
+            f"Accuracy (mean±std over folds): {res_before['accuracy_mean_over_folds']:.4f} ± {res_before['accuracy_std_over_folds']:.4f}")
+        for i, lab in enumerate(res_before["labels"]):
+            print(f"Class {lab}  Precision: {res_before['precision_per_class'][i]:.4f}  "
+                  f"Recall: {res_before['recall_per_class'][i]:.4f}  F1: {res_before['f1_per_class'][i]:.4f}")
+        print(f"Macro Precision: {res_before['macro_precision']:.4f}  "
+              f"Macro Recall: {res_before['macro_recall']:.4f}  Macro F1: {res_before['macro_f1']:.4f}")
+
+        # Save results
+        np.save(f"result/confusion_matrix_on_{filename}_before.npy", res_before["confusion_matrix"])
+        print()
 
 
-    # After pruning (if enabled)
+    # Have pruning, train, evaluate, prune and evaluate
     if args.prune:
-        print("\n=== After pruning ===")
+        print("\n=== Pruning to be applied ===")
         res_after = cross_validate_pruned_tree(
-            X, y, DecisionTree, filename=filename, show_trees=args.show,
+            X, y, DecisionTree, filename=filename,
             n_splits=args.folds, seed=args.seed,
             max_depth=args.max_depth, threshold=args.threshold
         )
@@ -96,8 +98,18 @@ def main():
         np.save(f"result/confusion_matrix_on_{filename}_prune_experiment_before.npy", res_after["confusion_matrix_before_pruning"])
         np.save(f"result/confusion_matrix_on_{filename}_prune_experiment_after.npy", res_after["confusion_matrix"])
 
-    # Save results
-    np.save(f"result/confusion_matrix_on_{filename}_before.npy", res_before["confusion_matrix"])
+    # Bonus. Train a decision tree on the whole clean dataset and plot the tree
+    # load data
+    data = load_dataset('data/clean_dataset.txt')
+    X = data[:, :-1]
+    y = np.round(data[:, -1]).astype(int)
+    # normalization
+    X_mean, X_std = X.mean(axis=0), X.std(axis=0)
+    X = (X - X_mean) / X_std
+    # train decision tree and plot tree
+    tree_whole_clean_dataset = DecisionTree(X, y, y)
+    tree_whole_clean_dataset = tree_whole_clean_dataset.fit(args.max_depth, args.threshold)
+    plot_decision_tree(tree_whole_clean_dataset, save_name_prefix='Tree_on_whole_clean_dataset')
 
 if __name__ == "__main__":
     main()
